f1f98688ae1421470e88bc38d021586037d8e981
koukitakanami <takanami02829@gmail.com>
2017-08-21 04:04:20 +0000

    heroku

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.create(contacts_params)
     if @contact.save
-      redirect_to new_contact_path, notice:"お問い合わせありがとうございました！"
+      redirect_to  root_path, notice:"お問い合わせありがとうございました！"
     else
       # 入力フォームを再描画します。
       render 'new'
